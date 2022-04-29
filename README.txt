416cb9d94d08c64389ae56e6cae3d1a978c3513d
FrederikPosTheDev <57841098+FrederikPos@users.noreply.github.com>
2022-04-29 11:39:46 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+How to use:
+if you have'nt already then install python. Then just open your 
+command prompt/terminal and make sure you are in the directory where you have your
+conversion.py file and type "python conversion.py" then hit enter and boom! 
+That's it.
---
+++ conversion.py
@@ -0,0 +1,62 @@
+while True:
+ answer=input("Do you wanna convert some units! If no then type 'n' But If yes then what do you wanna convert with? Type one of the first letters in lowercase of the units to convert them (1. Celsius to Fahrenheit = c or f 2. Miles to Km = m or k):")
+ if answer.lower()=='c':
+   answer=input("F° to C° or C° to F° (Type the letters in lowercase and in order of how you want to convert e.g. fc for F° to C°)")
+   if answer.lower()=='cf':
+     Celsius = int(input("How much celsius do you wanna convert?"))
+     Fahrenheit = Celsius * 9 / 5 + 32
+     print("Your initial", Celsius, "Celsius is now", Fahrenheit, "Fahrenheit!")
+   else:
+    if answer.lower()=='fc':
+      Fahrenheit = int(input("How much Fahrenheit do you wanna convert?"))
+      Celsius = (Fahrenheit - 32) * 5 / 9
+      print("Your initial", Fahrenheit, "Fahrenheit is now", Celsius, "Celsius!")
+    else:
+      print("I did'nt get that. Please open up the script again and try again.")
+      quit()
+ else:
+   if answer.lower()=='f':
+     answer=input("F° to C° or C° to F° (Type the letters in lowercase and in order of how you want to convert e.g. fc for F° to C°)")
+     if answer.lower()=='cf':
+       Celsius = int(input("How much celsius do you wanna convert?"))
+       Fahrenheit = Celsius * 9 / 5 + 32
+       print("Your initial", Celsius, "Celsius is now", Fahrenheit, "Fahrenheit!")
+     else:
+      if answer.lower()=='fc':
+        Fahrenheit = int(input("How much Fahrenheit do you wanna convert?"))
+        Celsius = (Fahrenheit - 32) * 5 / 9
+        print("Your initial", Fahrenheit, "Fahrenheit is now", Celsius, "Celsius!")
+      else:
+        print("I did'nt get that. Please open up the script again and try again.")
+        quit()
+
+
+ if answer.lower()=='m':
+     answer=input("Kilometers to Miles or Miles to Kilometers (Type the letters in lowercase and in order of how you want to convert e.g. km for Kilometers to Miles)")
+     if answer.lower()=='km':
+       Kilometers = int(input("How many Kilometers do you wanna convert?"))
+       Miles = Kilometers / 1.609344
+       print("Your initial", Celsius, "Celsius is now", Fahrenheit, "Fahrenheit!")
+     else:
+      if answer.lower()=='mk':
+        Miles = int(input("How many Miles do you wanna convert?"))
+        Kilometers = Miles * 1.609344
+        print("Your initial", Miles, "Miles is now", Kilometers, "Celsius!")
+      else:
+        print("I did'nt get that. Please open up the script again and try again.")
+        quit()
+ else:
+   if answer.lower()=='k':
+     answer=input("Kilometers to Miles or Miles to Kilometers (Type the letters in lowercase and in order of how you want to convert e.g. km for Kilometers to Miles)")
+     if answer.lower()=='km':
+       Kilometers = int(input("How many Kilometers do you wanna convert?"))
+       Miles = Kilometers / 1.609344
+       print("Your initial", Celsius, "Celsius is now", Fahrenheit, "Fahrenheit!")
+     else:
+      if answer.lower()=='mk':
+        Miles = int(input("How many Miles do you wanna convert?"))
+        Kilometers = Miles * 1.609344
+        print("Your initial", Miles, "Miles is now", Kilometers, "Celsius!")
+      else:
+        print("I did'nt get that. Please open up the script again and try again.")
+        quit()
